bd7c6c54e4e018afda1a3979fd9f875cc9346929
Alyssa20lopez <alyssa20lopez@gmail.com>
2022-09-18 12:56:14 -0400

    Added values

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,17 +1,30 @@
 INSERT INTO department (department_name)
 VALUES
     ('Customer Engineering');
+    ('Management');
+    ('Human Resources');
+    ('Finance');
+    ('Engineering');
+   
 
 SELECT * FROM department;
 
 INSERT INTO position (title, salary, department_id)
 VALUES
-    ('Software Engineer', 125,000.00, 05);
+    ('Software Engineer', 125,000.00, 020);
+    ('Account Manager', 200,000.00, 010);
+    ('Financial Analyst', 130,000.00, 030);
+    ('Internal Auditor', 170,000.00, 031);
+    ('Technical Solutions Engineer', 167,000.00, 022);
 
 SELECT * FROM position;
 
 INSERT INTO employee (first_name, last_name, position_id, manager_id)
 VALUES
-    ('Jane', 'Doe', 150);
+    ('Jane', 'Doe', 150, null);
+    ('Sarah', 'Love', 150, null);
+    ('Aaron', 'Applewood', 150, null);
+    ('Richard', 'Stewart', 150, null);
+    ('Salem', 'Price', 150, null);
 
 SELECT * FROM employee;
