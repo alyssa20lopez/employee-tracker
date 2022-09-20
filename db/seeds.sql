5fd8b8153db011b40c25271eacafb6d02e92600c
Alyssa20lopez <alyssa20lopez@gmail.com>
2022-09-19 23:37:28 -0400

    Updated seeds.sql

--- db/seeds.sql
+++ db/seeds.sql
@@ -10,14 +10,14 @@ INSERT INTO role (title, salary, department_id)
 VALUES
     ('Software Engineer', 125000.00, 1),
     ('Account Manager', 200000.00, 2),
-    ('Financial Analyst', 130000.00, 3),
+    ('Resources Manager', 90000.00, 3),
     ('Internal Auditor', 170000.00, 4),
     ('Technical Solutions Engineer', 167000.00, 5);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES
     ('Jane', 'Doe', 1, null),
-    ('Sarah', 'Love', 2, null),
-    ('Aaron', 'Applewood', 3, null),
+    ('Sarah', 'Love', 2, 3),
+    ('Aaron', 'Applewood', 3, 2),
     ('Richard', 'Stewart', 4, null),
     ('Salem', 'Price', 5, null);
